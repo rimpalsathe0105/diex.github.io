3a8627546e18b0686b6efd94951bc24a36435ec9
Mayurdhvaj15081996 <rajmayurdhvaj9@gmail.com>
2021-06-27 20:06:37 +0530

    Changes
    Made in CSS File, App.js, SignUp.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './Header';
 import Navigation from './Navigation';
 //import Footer from '/.Footer';
+import SignUp from './SignUp';
 
 function App() {
   return (
     <div className="App">
         <Header/>
         <Navigation/>
+        <SignUp/>
     </div>
   );
 }

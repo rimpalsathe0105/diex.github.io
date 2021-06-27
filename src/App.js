aa2af3d77b55d3984bbd97f73c3af25e27034649
Mayurdhvaj15081996 <rajmayurdhvaj9@gmail.com>
2021-06-27 19:38:35 +0530

    Added Few Files
    Footer.js
    LogInForm.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './Header';
 import Navigation from './Navigation';
+//import Footer from '/.Footer';
 
 function App() {
   return (
--- src/Navigation.js
+++ src/Navigation.js
@@ -3,12 +3,7 @@ import React from 'react';
 export default function Header(){
         return(
             <div className = "Navigation" Method = "POST">
-                <form className = "buttons">
-                    <input type = "button" className = "Home" Value = "Home"/>
-                    <input type = "button" className = "Home" Value = "Examination"/>
-                    <input type = "button" className = "Home" Value = "Contact Us" onClick="contact.html"/>
-                </form>
-                
+              
             </div>
         );
 }

61d323ce37a5618f424662d477045fac13908247
Mayurdhvaj15081996 <rajmayurdhvaj9@gmail.com>
2021-06-27 19:42:46 +0530

    File Name Changed
    File name changed from LogInForm.js to SignUp.js

---
+++ src/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SignUp(){
+        return(
+            <div className = "Login">
+              <form id = "signup" method = "POST">
+                  
+              </form>
+            </div>
+        );
+}

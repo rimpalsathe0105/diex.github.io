2b9d7430a624acc07e95ce54d362f13d64f95fc3
Mayurdhvaj15081996 <rajmayurdhvaj9@gmail.com>
2021-07-01 23:57:09 +0530

    Changes
    I Added an Image
    Made Changes in CSS file and SignUp.js

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import image from './forMainPage.png';
 
-export default function SignUp(){
-        return(
-            <div className = "Sign_Up">
-             <h1> <marquee>Coming Soon</marquee> </h1>
+export default function SignUp() {
+    return (
+        <div className="LogIn">
+            <h1> <marquee>Coming Soon</marquee> </h1>
+            <div className="Logo_Section">
+                <img src={image} width="600" height="500" className="Logo"></img>
             </div>
-        );
+            <div className="Log in">
+                <table className="ForGButton" align="center">
+                    <tr>
+                    
+                    </tr>
+                </table>
+            </div>
+        </div>
+    );
 }

7c7ecce421e1ffd3e14306bdcdf0d3290875f1a8
Mayurdhvaj15081996 <rajmayurdhvaj9@gmail.com>
2021-07-01 20:56:18 +0530

    Changes
    2 Logos are deleted
    Marquee added in SignUp.js

--- src/SignUp.js
+++ src/SignUp.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function SignUp(){
         return(
             <div className = "Sign_Up">
-              
+             <h1> <marquee>Coming Soon</marquee> </h1>
             </div>
         );
 }
